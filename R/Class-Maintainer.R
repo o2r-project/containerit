5ea8783a6195bbd8cdb95ea8fb87e821096e30db
nuest <daniel.nuest@uni-muenster.de>
2017-05-23 16:35:35 +0200

    set maintainer deprecated

--- R/Class-Maintainer.R
+++ R/Class-Maintainer.R
@@ -3,11 +3,9 @@
 #' An S4 class to represent a Dockerfile's maintainer
 #' @include Class-Instruction.R
 #' @include Class-Label.R
-#' 
+#'
 #' See official documentation at \url{https://docs.docker.com/engine/reference/builder/#maintainer}.
-#' 
-#' TODO: The Maintainer field is deprecated, use Label_Maintainer instead
-#' 
+#'
 #' @slot name The name
 #' @slot email The email
 #'
@@ -15,36 +13,39 @@
 #' @family maintainer
 #' @return an object of class \code{Maintainer}
 #' @export
-setClass("Maintainer",
-         slots = list(name = "character",
-                      email = "character"),
-         contains = "Instruction")
-
+setClass(
+  "Maintainer",
+  slots = list(name = "character",
+               email = "character"),
+  contains = "Instruction"
+)
 
 #' create objects of class Maintainer
-#' 
-#' TODO: The Maintainer field is deprecated, use Label_Maintainer instead
-#' 
+#'
 #' @param name character name (by convention: "<given name> <last name>", e.g. "Matthias Hinz")
 #' @param email The email
-#' 
+#'
 #' @family maintainer
 #' @return Maintainer object
 #' @export
 #'
-Maintainer <- function(name, email = NA_character_){
+Maintainer <- function(name, email = NA_character_) {
+  .Deprecated("Label_Maintainer")
   new("Maintainer", name = name, email = email)
 }
 
-##The maintainer is optional in a dockerfile
-setClassUnion("NullOrLabelOrMaintainer", members = c("Maintainer", "NULL", "Label"))
+# The maintainer is optional in a dockerfile
+setClassUnion("NullOrLabelOrMaintainer",
+              members = c("Maintainer", "NULL", "Label"))
 
-##Specify how class slots are composed to instruction arguments
-setMethod("docker_arguments", signature = signature(obj = "Maintainer"),
-          definition = function(obj){
-            arg <- paste0("\"", obj@name, "\"")
-            if (!is.na(obj@email) && length(obj@email) > 0)
-              arg <- paste(arg, obj@email)
-            return(arg)
-          }
+# Specify how class slots are composed to instruction arguments
+setMethod(
+  "docker_arguments",
+  signature = signature(obj = "Maintainer"),
+  definition = function(obj) {
+    arg <- paste0("\"", obj@name, "\"")
+    if (!is.na(obj@email) && length(obj@email) > 0)
+      arg <- paste(arg, obj@email)
+    return(arg)
+  }
 )
